3d8af79a0988b5408ee51711c2ec26b042b4b508
Rob Anderson <robandpdx@github.com>
2023-03-29 10:21:47 -0600

    az-container module

---
+++ az-container/main.tf
@@ -0,0 +1,49 @@
+variable "resource_group_name" {
+  description = "Name of the resource group where the container instance will be deployed."
+}
+
+variable "location" {
+  description = "Location where the container instance will be deployed."
+}
+
+variable "container_image" {
+  description = "Docker image to use for the container instance."
+}
+
+variable "container_name" {
+  description = "Name to use for the container instance."
+}
+variable "dns_prefix" {
+  description = "DNS prefix to use for the container instance."
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_group" "container" {
+  name                = var.container_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_address_type     = "Public"
+  dns_name_label      = "${var.dns_prefix}"
+  os_type = "Linux"
+
+  container {
+    name   = var.container_name
+    image  = var.container_image
+    cpu    = "1"
+    memory = "1.5"
+
+    ports {
+      port     = 3000
+      protocol = "TCP"
+    }
+  }
+}
+
+output "container_instance_fqdn" {
+  value = azurerm_container_group.container.fqdn
+}
